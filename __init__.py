f7006abeabdfb6454ba163e1748d81112cae65d4
Phlya <flyamer@gmail.com>
2018-12-13 17:56:56 +0000

    Allow importing main functions from python

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from coolpup.py import averageLoopsWithControl
+from coolpup.py import averageLoopsByWindowWithControl
+from coolpup.py import averageLoopsByWindow
+from coolpup.py import averageLoops
