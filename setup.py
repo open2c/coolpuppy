c9cc89053fbfe81d3d9730bad5bed5a2bc1002e2
Phlya <flyamer@gmail.com>
2019-03-08 14:25:34 +0000

    Fix duplicate dropping

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='coolpuppy',
-      version='0.7.1',
+      version='0.7.2',
       scripts=['coolpup.py'],
       install_requires=['numpy', 'cooler', 'pandas', 'natsort', 'scipy',
                         'mirnylib', 'cooltools']
