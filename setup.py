32d2c9eec92fe0160c701901c8d9c46b3e0cb148
Phlya <flyamer@gmail.com>
2019-03-20 16:52:27 +0000

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
       name='coolpuppy',
-      version='0.7.5',
+      version='0.7.6',
       scripts=['coolpup.py'],
       install_requires=['numpy', 'cooler', 'pandas', 'natsort', 'scipy',
-                        'cooltools']
+                        'cooltools'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      project_urls={'Source':'https://github.com/Phlya/coolpuppy',
+                    'Issues':'https://github.com/Phlya/coolpuppy/issues'}
 )
