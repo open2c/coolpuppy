cb3f51ae2b6f55a7de0a64b50e8b3b1a51b83bc3
Phlya <flyamer@gmail.com>
2019-03-24 18:02:40 +0000

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
       name='coolpuppy',
-      version='0.8.1',
+      version='0.8.2',
       scripts=['coolpup.py'],
       install_requires=['numpy', 'cooler', 'pandas', 'natsort', 'scipy',
                         'cooltools'],
