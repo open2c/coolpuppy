30b5288033d792873aa36c96cf51f4674c47122d
Phlya <flyamer@gmail.com>
2019-05-15 11:51:06 +0100

    Bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
       name='coolpuppy',
-      version='0.8.5',
+      version='0.8.6',
       packages=['coolpuppy'],
       entry_points={
           'console_scripts': ['coolpup.py = coolpuppy.__main__:main',
