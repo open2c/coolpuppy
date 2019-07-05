df9d0ae105e3243d4bf617150f596fedb92a01d0
Phlya <flyamer@gmail.com>
2019-07-05 20:47:39 +0100

    More testing

--- tests/test_coolpup.py
+++ tests/test_coolpup.py
@@ -15,14 +15,15 @@ import pytest
 
 
 amap = np.loadtxt('tests/Scc1-control.10000-10.0K_over_CH12_loops_Rao_10-shifts_dist_100000-inf_unbalanced_covnorm.np.txt')
+amapTAD = np.loadtxt('tests/Scc1-control.10000-10.0K_over_CH12_TADs_Rao_10-shifts_local_rescaled_unbalanced_covnorm.np.txt')
 
 def test_cornerCV():
-    assert np.isclose(cornerCV(amap), 0.07768632150324585)
-    assert np.isclose(cornerCV(amap, 3), 0.06738199521452154)
+    assert np.isclose(cornerCV(amap), 0.0752115185866688)
+    assert np.isclose(cornerCV(amap, 3), 0.07433758192350227)
 
 def test_get_enrichment():
-    assert np.isclose(get_enrichment(amap, 1), 1.6003706696232056)
-    assert np.isclose(get_enrichment(amap, 3), 1.4221401510110887)
+    assert np.isclose(get_enrichment(amap, 1), 1.6307401942981528)
+    assert np.isclose(get_enrichment(amap, 3), 1.4057840415780747)
 
 bed = pd.read_csv('tests/test.bed', sep='\t', names=['chr', 'start', 'end'])
 
@@ -40,3 +41,23 @@ def test_auto_read_bed():
     assert np.all(auto_read_bed('tests/test.bed') == bed)
     assert np.all(auto_read_bed('tests/test.bedpe') == bedpe)
 
+def test_get_mids():
+    bed_mids = get_mids(bed, 1000, bed=True)
+    assert np.all(bed_mids['Bin'] == [1, 12, 2, 1])
+    assert np.all(bed_mids['Pad'] == [500, 10000, 1000, 500])
+
+    bedpe_mids = get_mids(bedpe, 1000, bed=False)
+    assert np.all(bedpe_mids['Bin1'] == [1, 21, 1, 101])
+    assert np.all(bedpe_mids['Bin2'] == [3, 51, 1, 201])
+    assert np.all(bedpe_mids['Pad1'] == [100, 1000, 150, 1000])
+    assert np.all(bedpe_mids['Pad2'] == [100, 1000, 50, 1000])
+
+
+def test_pileupsWithControl():
+    np.random.seed(0)
+    loops = auto_read_bed('tests/CH12_loops_Rao.bed')
+    loopmids = get_mids(loops, resolution=10000, bed=False)
+    pup = pileupsWithControl(loopmids, 'tests/Scc1-control.10000.cool',
+                             mindist=100000, balance=False, cov_norm=True,
+                             bed=False, pad=10)
+    assert np.allclose(pup, amap)
