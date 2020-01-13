dbf02932d8542ea3251cf8709c9cb3d7c763b8f6
Phlya <flyamer@gmail.com>
2020-01-13 19:22:25 +0000

    Fix docs?

--- coolpuppy/plotpup.py
+++ coolpuppy/plotpup.py
@@ -5,17 +5,49 @@
 import numpy as np
 
 
-def norm_cis(amap, i=3):
-    return amap / np.nanmean((amap[0:i, 0:i] + amap[-i:, -i:])) * 2
-
-
 def auto_rows_cols(n):
+    """Automatically determines number of rows and cols for n pileups
+
+    Parameters
+    ----------
+    n : int
+        Number of pileups.
+
+    Returns
+    -------
+    rows : int
+        How many rows to use.
+    cols : int
+        How many columsn to use.
+
+    """
     rows = int(np.ceil(np.sqrt(n)))
     cols = int(np.ceil(n / rows))
     return rows, cols
 
 
 def get_min_max(pups, vmin=None, vmax=None, sym=True):
+    """Automatically determine minimal and maximal colour intensity for pileups
+
+    Parameters
+    ----------
+    pups : list
+        List of numpy arrays conaining pileups.
+    vmin : float, optional
+        Force certain minimal colour. The default is None.
+    vmax : float, optional
+        Force certain maximal colour. The default is None.
+    sym : bool, optional
+        Whether the output should be cymmetrical around 0. The default is True.
+
+    Returns
+    -------
+    vmin : float
+        Selected minimal colour.
+    vmax : float
+        Selected maximal colour.
+
+    """
     if vmin is not None and vmax is not None:
         return vmin, vmax
     else:
