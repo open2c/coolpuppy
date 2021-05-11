242383b2e2b3e7473b451c096a13f15f00ee149b
Phlya <flyamer@gmail.com>
2021-05-11 09:15:52 +0100

    Add regions to CLI

--- coolpuppy/__main__.py
+++ coolpuppy/__main__.py
@@ -48,6 +48,13 @@ def parse_args_coolpuppy():
         required=False,
     )
     parser.add_argument(
+        "--regions",
+        type=str,
+        help="""Path to a file which defines which regions of the chromosomes to use""",
+        default=None,
+        required=False,
+    )
+    parser.add_argument(
         "--pad",
         default=100,
         type=int,
@@ -338,6 +345,13 @@ def main():
         bedname = "stdin"
         baselist = bf.read_table(sys.stdin, schema=schema, index_col=False)
         bedname += "_vs_" + os.path.splitext(os.path.basename(args.bed2))[0]
+    
+    if args.regions is not None:
+        regions = pd.read_csv(
+            args.regions, sep="\t", names=["chrom", "start", "end", "name"]
+        )
+    else:
+        regions = None
 
     if args.nshifts > 0:
         control = True
@@ -425,6 +439,7 @@ def main():
     PU = PileUpper(
         clr=c,
         CC=CC,
+        regions=regions,
         balance=balance,
         expected=expected,
         ooe=args.ooe,
